UtsavBalar1231/kernel_rockchip_linux

bluetooth: serdev: Fix undefined reference to hci_uart_init_work
Change-Id: I0f9f077237f001751e3f609406351e0a4f45eab9
Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

drivers/bluetooth/hci_serdev.c
@@ -327,7 +327,9 @@ int hci_uart_register_device(struct hci_uart *hu,
 	hdev->bus = HCI_UART;
 	hci_set_drvdata(hdev, hu);
 
+#if !IS_ENABLED(CONFIG_RTKBT)
 	INIT_WORK(&hu->init_ready, hci_uart_init_work);
+#endif
 	INIT_WORK(&hu->write_work, hci_uart_write_work);
 
 	/* Only when vendor specific setup callback is provided, consider

avi951/rk3399_kernel_6.1

drivers/bluetooth/hci_serdev.c
@@ -327,7 +327,9 @@ int hci_uart_register_device(struct hci_uart *hu,
 	hdev->bus = HCI_UART;
 	hci_set_drvdata(hdev, hu);
 
+#if !IS_ENABLED(CONFIG_RTKBT_UART)
 	INIT_WORK(&hu->init_ready, hci_uart_init_work);
+#endif
 	INIT_WORK(&hu->write_work, hci_uart_write_work);
 
 	/* Only when vendor specific setup callback is provided, consider